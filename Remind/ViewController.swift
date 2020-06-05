56d1871af25ee20e751a4f3d3c1e6aafcc669857
Hrayr2112 <hrayr.yeghiazaryan@gmail.com>
2020-06-06 02:48:27 +0400

    Initial Commit

---
+++ Remind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remind
+//
+//  Created by Hrayr Yeghiazaryan on 06.06.2020.
+//  Copyright © 2020 Remind. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
