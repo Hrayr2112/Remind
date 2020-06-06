cebf84cbe6de7508adfb1bd4fabfd6082e597aaf
Hrayr2112 <hrayr.yeghiazaryan@gmail.com>
2020-06-07 01:15:15 +0400

    Navigation bar hidden

--- Remind/Main/ClassroomViewController.swift
+++ Remind/Main/ClassroomViewController.swift
@@ -53,6 +53,16 @@ class ClassroomViewController: UIViewController {
         configureCollectionView()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+    
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         if !isAnimating {
