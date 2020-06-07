7e821052d23bb2b8ae2f924fd88cbad78537a68a
Aram Sargsyan <aram.sargsyan.1997@gmail.com>
2020-06-07 12:30:04 +0400

    Changed one request

--- Remind/Main/MeViewController.swift
+++ Remind/Main/MeViewController.swift
@@ -60,19 +60,54 @@ class MeViewController: UIViewController {
             guard let username = UserManager.shared.username,
                 let userId = UserManager.shared.id,
                 let imageBase64 = image.pngData()?.base64EncodedString() else { return }
-            RequestService().uploadImage(name: username.appending(UUID().uuidString).appending(".png"),
-                                         content: imageBase64,
-                                         userId: userId)
-            { uploadImageResponse in
-                switch uploadImageResponse {
-                case .success(let response):
-                    UserManager.shared.set(imageId: response.image.id)
-                case .failure(let error):
-                    let errorAc = UIAlertController(title: error.localizedDescription, message: nil, preferredStyle: .alert)
-                    errorAc.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-                    self.present(errorAc, animated: true, completion: nil)
+            
+            guard let token = UserManager.shared.token,
+                let classroomId = UserManager.shared.classroomId else { return }
+            
+            let bearerToken = "Bearer" + " " + token
+            let Url = String(format: "http://89.208.220.86/api/v1/users/\(userId)/images")
+            guard let serviceUrl = URL(string: Url) else { return }
+            if let parameterDictionary = ["name" : username.appending(UUID().uuidString).appending(".png"),
+                                          "content": imageBase64] as? [String: Any] {
+                var request = URLRequest(url: serviceUrl)
+                request.httpMethod = "POST"
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+                request.setValue("application/json", forHTTPHeaderField: "Accept")
+                request.setValue(bearerToken, forHTTPHeaderField: "Authorization")
+                guard let httpBody = try? JSONSerialization.data(withJSONObject: parameterDictionary, options: []) else {
+                    return
                 }
+                request.httpBody = httpBody
+                
+                let session = URLSession.shared
+                session.dataTask(with: request) { (data, response, error) in
+                    if let response = response {
+                        print(response)
+                    }
+                    if let data = data {
+                        do {
+                            let json = try JSONSerialization.jsonObject(with: data, options: [])
+                            print(json)
+                        } catch {
+                            print(error)
+                        }
+                    }
+                }.resume()
             }
+            
+//            RequestService().uploadImage(name: username.appending(UUID().uuidString).appending(".png"),
+//                                         content: imageBase64,
+//                                         userId: userId)
+//            { uploadImageResponse in
+//                switch uploadImageResponse {
+//                case .success(let response):
+//                    UserManager.shared.set(imageId: response.image.id)
+//                case .failure(let error):
+//                    let errorAc = UIAlertController(title: error.localizedDescription, message: nil, preferredStyle: .alert)
+//                    errorAc.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+//                    self.present(errorAc, animated: true, completion: nil)
+//                }
+//            }
             self.avatarImageView.image = image
         }
         present(cameraViewController, animated: true, completion: nil)
